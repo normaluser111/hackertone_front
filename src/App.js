411da2ab2f7f568594c57fbb785e990bde05549b
normaluser111 <js6088@naver.com>
2023-08-17 23:55:45 +0900

    front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import MapComponent from './MapComponent';
+import dummyData from './dummyData';
+import userData from './userData';
 
 function App() {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    // 사용자의 위치 정보를 받아와 userData 업데이트
+    userData.userlatitude = 37.234567; // 사용자의 위도 데이터
+    userData.userlongitude = 128.876543; // 사용자의 경도 데이터
+    userData.places = 1; // 사용자가 선택한 가게 종류 등의 정보
+
+    // 더미 데이터를 임시로 사용
+    setPlaces(dummyData);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapComponent userlatitude={userData.userlatitude} userlongitude={userData.userlongitude} places={places} />
     </div>
   );
 }
 
 export default App;
+//index.html, MapComponent.js, userData.js, dummyData.js, App.js만 썻습니다.
---
+++ src/userData.js
@@ -0,0 +1,9 @@
+const userData = {
+    userlatitude: null,
+    userlongitude: null,
+    places: null
+  };
+  
+  export default userData;
+  
+  
